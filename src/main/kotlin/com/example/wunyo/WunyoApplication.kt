49815a9b21d8610615b9374145967a9c91b4c50c
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-07-04 14:32:28 +0900

    Setting :: Project Setting

---
+++ src/main/kotlin/com/example/wunyo/WunyoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.wunyo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WunyoApplication
+
+fun main(args: Array<String>) {
+    runApplication<WunyoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/wunyo/WunyoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.wunyo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WunyoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
